bb334bca8d80ca9c6adb956058d204f0da711368
Глеб Лысов <barabashka.zzz@gmail.com>
2016-03-29 23:39:12 +0300

    deleted

--- ConsoleApplication2/ConsoleApplication2/list.cpp
+++ ConsoleApplication2/ConsoleApplication2/list.cpp
@@ -5,87 +5,136 @@
 using namespace std;
 
 
+elements::elements()
+{
+	next = nullptr;
+	data = "";
+}
 list::list()
 {
-	
-	next = nullptr;
-	info = "";
+	e = new elements;
 }
-
+list::~list()
+{
+	while (e->next != nullptr)
+	{
+		elements *t = e->next; 
+		delete e;
+		e = t;
+	}
+}
 void list::add(int i, string data) {
+	elements *temp;
+	temp = e;
 
 	if (i == 0)
 	{
-		this->info = data;
+		temp->data = data;
 	}
 	else
 	{
-		if (this->next == nullptr)
+		if (temp->next == nullptr)
 		{
-			this->next = new list();
-			this->next->add(i - 1, data);
+			temp->next = new elements();
+			temp = temp->next;
+			add(i - 1, data);
 		}
 		else
 		{
-			this->next->add(i - 1, data);
+			add(i - 1, data);
 		}
 	}
 }
 
-void list::get(int i)
+string list::get(int i)
 {
-	if (next == nullptr && info == "")  //чекаем пустой список или нет
-	{
-		cout << "list is empty" << endl;
-		return;
-	}
-	bool empty = false; //чекаем создан элемент или нет
-	list *pp = this;
+	elements *temp;
+	temp = e;
 	for (int j = 0; j < i; j++)
 	{
-		if (pp->next == nullptr&& j != 0)
-		{
-			empty = true;
-			break;
-		}
-		pp = pp->next;
+		//if (temp->next == nullptr) break;
+		temp = temp->next;
 	}
-	if (!empty)
-	{
-		cout << pp->info.data() << endl;
-	}
-	else
-	{
-		cout << "Index not created" << endl;
-	}
+	return (temp->data);
+	//if (next == nullptr && info == "")  //чекаем пустой список или нет
+	//{
+	//	cout << "list is empty" << endl;
+	//	return 0;
+	//}
+	//bool empty = false; //чекаем создан элемент или нет
+	//list *pp = this;
+	//for (int j = 0; j < i; j++)
+	//{
+	//	if (pp->next == nullptr&& j != 0)
+	//	{
+	//		empty = true;
+	//		break;
+	//	}
+	//	pp = pp->next;
+	//}
+	//if (!empty)
+	//{
+	//	return(pp->info.data());
+	//}
+	//else
+	//{
+	//	return 0;
+	//}
 
 }
 
 void list::del(int i)
 {
-	list *p = this;
-	list *pp;
-	if (next == nullptr && info == "")
+	if (e->next == nullptr && e->data == "")  return;
+	elements *temp;
+	temp = e;
+	if (i == 0)
 	{
-		cout << "list is empty" << endl;
-		return;
-	}
-	if (i = 0)   //удаление со сдвигом т.е. был 5 элемент = 10, при удалении 3 элемента, 4 элемент = 10
-	{			 //а 5 больше не существует
-		pp = p;
-		p = p->next;
-		delete pp;
+		if (e->next == nullptr)
+		{
+			e->data = "";
+		}
+		else
+		{
+			e = e->next;
+			delete temp;
+		}
 	}
 	else
 	{
+		elements *temp2;
 		for (int j = 0; j < i - 1; j++)
 		{
-
-			p = p->next;
+			temp = temp->next;
 		}
-		pp = p;			//сохраняем предыдущий элемент
-		p = p->next;
-		pp->next = p->next;  //меняем указатель на предыдущем на указатель на следующий
-		delete p;
+		temp2 = temp;
+		temp = temp->next;
+		temp2->next = temp->next;
+		delete temp;
 	}
+	//list *p = this;
+	//list *pp;
+	//if (next == nullptr && info == "")
+	//{
+	//	cout << "list is empty" << endl;
+	//	return;
+	//}
+	//if (i = 0)   //удаление со сдвигом т.е. был 5 элемент = 10, при удалении 3 элемента, 4 элемент = 10
+	//{			 //а 5 больше не существует
+	//	pp = p;
+	//	p = p->next;
+	//	delete pp;
+	//}
+	//else
+	//{
+	//	for (int j = 0; j < i - 1; j++)
+	//	{
+
+	//		p = p->next;
+	//	}
+	//	pp = p;			//сохраняем предыдущий элемент
+	//	p = p->next;
+	//	pp->next = p->next;  //меняем указатель на предыдущем на указатель на следующий
+	//	delete p;
+	//}
 }
--- ConsoleApplication2/ConsoleApplication2/list.h
+++ ConsoleApplication2/ConsoleApplication2/list.h
@@ -3,13 +3,23 @@
 #include <string>
 using namespace std;
 
+
+class elements {
+private:
+	string data;
+	elements *next;
+public:
+	elements();
+	friend class list;
+};
+
 class list {
 private:
-	string info;
-	list *next;
+	elements *e;
 public:
 	list();
+	~list();
 	void add(int i, string data);
-	void get(int i);
+	string get(int i);
 	void del(int i);
 };
