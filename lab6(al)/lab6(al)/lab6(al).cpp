0a32cdaa5b62c168c2046099e60a00396fea67fd
Глеб Лысов <barabashka.zzz@gmail.com>
2016-06-09 21:06:34 +0300

    iteratori

--- lab6(al)/lab6(al)/lab6(al).cpp
+++ lab6(al)/lab6(al)/lab6(al).cpp
@@ -4,6 +4,9 @@
 #include "stdafx.h"
 #include <iostream>
 #include "ctime"
+#include <memory>
+#include <algorithm>
+
 using namespace std;
 
 //class rational {
@@ -19,314 +22,187 @@ using namespace std;
 //	const rational operator*(int lambda); //умножение на число
 //};
 
-template <class T>
+template <class T, int m, int n>
 class matrix {
 private:
 	T **mat;
 public:
-	int m; //stroki
-	int n; //stolbci
-	matrix<T>(int m, int n);
-	const matrix<T> operator*(const matrix<T>& mat);  //умножение матриц
-	const matrix<T> operator+(const matrix<T>& mat); //plus
-	const matrix<T> operator|(const matrix<T>& mat);
-	int set(int m, int n, T set);
-	//const T get(int m, int n);
-	//const matrix<T> rotate();
+	matrix<T, m, n>();
+	matrix(matrix &matr) {
+		mat = new T*[m];
+		for (int i = 0; i < m; i++)
+		{
+			mat[i] = new T[n];
+			for (int j = 0; j < n; j++) {
+				set(i, j, matr.get(i, j));
+			}
+		}
+
+	}
+	~matrix();
+
+
+	const void operator=(const matrix<T, m, n>& mat);
+
+	template <class U = T, int m2 = n, int n2>
+	matrix<T, m, n2> operator*(matrix<U, m2, n2>& mat);  //умножение матриц
+
+	T get(int m, int n);
+
+	template <class U = T, int m2 = m, int n2, int n3=n+n2>
+	
+	matrix<U, m2, n3> operator|(matrix<U, m2, n2>& mat);
+	
+	void set(int m_in, int n_in, T set);
 	void print(); //вывод матрицы
-	T begin();
-	T end();
-	class itr {
-	private:
-		int mpos;
-		int npos;
+	
+	
+	class iter {
+		matrix* p_mat;
+		int pos_m, pos_n;
 	public:
-		itr() {
-			mpos = 0;
-			npos = 0;
+		iter& operator++() {
+			if (pos_n == n - 1) {
+				pos_m++;
+				pos_n = 0;
+			}
+			else
+			{
+				pos_n++;
+			}
+			return *this;
+		}
+		bool operator!=(const iter& prev_it){
+			return !(pos_m == prev_it.pos_m && pos_n == prev_it.pos_n);
+		}
+		iter(matrix* p_mat, int pos_m, int pos_n) {
+			this->p_mat = p_mat;
+			this->pos_m = pos_m;
+			this->pos_n = pos_n;
 		}
 		T operator*() {
-			return mat[mpos][npos];
+			return p_mat->get(pos_m, pos_n);
 		}
-		T operator++() {
-			int old_mpos = mpos;
-			int old_npos = npos;
-			if (npos == (n - 1)) {
-				mpos++;
-			}
-			else {
-				npos++;
-			}
-			return mat[old_mpos][old_npos];
+		T* operator->() {
+			return *p_mat->get(pos_m, pos_n);
 		}
+
 	};
-	friend itr;
+	iter begin() {
+		iter temp(this, 0, 0);
+		return temp;
+	}
+	iter end() {
+		iter temp(this, m, 0);
+		return temp;
+	}
 };
-template <class T>
-matrix<T>::matrix(int m, int n) {
-	this->m = m;
-	this->n = n;
+template <class T, int m, int n>
+matrix<T, m, n>::matrix() {
 	mat = new T*[m];
 	for (int i = 0; i < m; i++)
 	{
 			mat[i] = new T[n];
 	}
 }
-template <class T>
-int matrix<T>::set(int m, int n, T set) {
-	mat[m-1][n-1] = set;
-	return 1;
+
+template <class T, int m, int n>
+T matrix<T, m, n>::get(int m, int n) {
+	return mat[m][n];
 }
-template <class T>
-const matrix<T> matrix<T>::operator*(const matrix<T>& mat)
+
+template <class T, int m, int n>
+const void matrix<T, m, n>::operator=(const matrix<T, m, n>& mat) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			set(i, j, mat.get(i, j));
+		}
+	}
+}
+
+template <class T, int m, int n>
+void matrix<T, m, n>::set(int m_in, int n_in, T set) {
+	mat[m_in][n_in] = set;
+}
+
+template <class T, int m, int n>
+template <class U = T, int m2 = n, int n2>
+matrix<T, m, n2> matrix<T, m, n>::operator*(matrix<U, m2, n2>& mat)
 {
-	matrix<T> temp = matrix<T>(m, mat.n); // создаем пустую матрицу
+	matrix<T, m, n2> temp; // создаем пустую матрицу m1xn2
 	T tempvalue; //tempvalue должен иметь возможность = 0 ))))
 	{
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < mat.n; j++)
+	for (int i = 0; i < m; i++) //m1
+		for (int j = 0; j < n2; j++) //n2
 		{
 			tempvalue = 0; 
-			for (int k = 0; k < n; k++)
+			for (int k = 0; k < n; k++) //n1
 			{
-				tempvalue = tempvalue + (this->mat[i][k] * mat.mat[k][j]);
+				tempvalue = tempvalue + (get(i, k) * mat.get(k, j));
 			}
-			temp.mat[i][j] = tempvalue;
+			temp.set(i, j, tempvalue);
 		}
 	}
 	return temp;
 }
-template <class T>
-const matrix<T> matrix<T>::operator+(const matrix<T>& mat)
-{
-	matrix<T> temp = matrix(m, n);
+
+template <class T, int m, int n> //class T должен быть перегружен на <<
+void matrix<T, m, n>::print() {
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			temp.mat[i][j] = this->mat[i][j] + mat.mat[i][j];
+			std::cout << get(i, j);
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+template <class T, int m, int n>
+template <class U = T, int m2 = m, int n2, int n3 = n + n2>
+matrix<U, m2, n3> matrix<T, m, n>::operator|(matrix<U, m2, n2>& mat) {
+	matrix<U, m2, n3> temp;
+	for (int i = 0; i < m2; i++) {
+		for (int j = 0; j < n; j++) {
+			temp.set(i, j, get(i, j));
+		}
+		for (int k = n; k < n + n2; k++) {
+			temp.set(i, k, mat.get(i, k - n));
 		}
 	}
 	return temp;
 }
-template <class T>
-const matrix<T> matrix<T>::operator|(const matrix<T>& mat)
-{
-	matrix<T> temp(m, n + mat.n);
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			temp.mat[i][j] = this->mat[i][j];
-		}
-		for (int k = n; k < (n + mat.n); k++) {
-			temp.mat[i][k] = mat.mat[i][k - n];
-		}
+
+template <class T, int m, int n>
+matrix<T, m, n>::~matrix() {
+	for (int i = 0; i < m; i++)
+	{
+		delete mat[i];
 	}
-	return temp;
+	delete mat;
 }
-//template <class T>
-//const rational matrix::get(int m, int n)
-//{
-//	T temp = mat[m - 1][n - 1];
-//	return temp;
-//}
-//template <class T>
-//const matrix matrix::rotate()
-//{
-//	matrix temp = matrix(m, n);
-//	for (int i = 0; i < m; i++)
-//	{
-//		for (int j = 0; j < n; j++)
-//		{
-//			temp.mat[i][j] = mat[m - i - 1][n - j - 1];
-//		}
-//	}
-//	return temp;
-//}
-//
-//template <class T> //class T должен быть перегружен на <<
-//void matrix::print() {
-//	for (int i = 0; i < m; i++)
-//	{
-//		for (int j = 0; j < n; j++)
-//		{
-//			std::cout<< mat[i][j];
-//			std::cout << " ";
-//		}
-//		std::cout << std::endl;
-//	}
-//}
-//
-template <class T>
-void matrix<T>::print()
-{
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << mat[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-//rational::rational() {
-//
-//}
-//rational::rational(int m, int n) {
-//	int a, b;
-//	a = m;
-//	b = n;
-//	while (a != 0 && b != 0)
-//	{
-//		if (a > b)
-//		{
-//			a = a%b;
-//		}
-//		else
-//		{
-//			b = b%a;
-//		}
-//	}
-//	a = a + b; // NOD
-//	this->m = m / a;
-//	this->n = n / a;
-//}
-//const rational rational::operator*(const rational& r) {
-//	rational temp = rational(m*r.m, n*r.n);
-//	return (temp);
-//}
-//const rational rational::operator*(int lambda) {
-//	rational temp = rational(m*lambda, n);
-//	return (temp);
-//}
-//const rational rational::operator+(const rational& r) {
-//	int a = n; // 1 znamenatel
-//	int b = r.n; // 2 znamenatel
-//	int nod;
-//	while (a != 0 && b != 0)
-//	{
-//		if (a > b)
-//		{
-//			a = a%b;
-//		}
-//		else
-//		{
-//			b = b%a;
-//		}
-//	}
-//	nod = a + b; // NOD
-//	a = n / nod;
-//	b = r.n / nod;
-//	rational temp = rational(m*b + r.m*a, nod*a*b);
-//	return temp;
-//}
-template <class T>
-T matrix<T>::begin() {
-	return mat[0][0];
-}
-template <class T>
-T matrix<T>::end() {
-	return mat[m - 1][n - 1];
-}
-//template <class T>
-//class matrix<T>::itr{
-//private:
-//	int mpos;
-//	int npos;
-//public:
-//	itr() {
-//		mpos = 0;
-//		npos = 0;
-//	}
-//	T operator*() {
-//		return mat[mpos][npos];
-//	}
-//	T operator++() {
-//		int old_mpos = mpos;
-//		int old_npos = npos;
-//		if (npos == (n - 1)) {
-//			mpos++;
-//		}
-//		else {
-//			npos++;
-//		}
-//		return mat[old_mpos][old_npos];
-//	}
-//}
 
 int main()
 {
-	//srand(time(0));
-	matrix<int> mmm(2, 2);
-	matrix<int> nnn(2, 2);
-	matrix<int>::itr sss;
-	mmm.set(1, 1, 1);
-	mmm.set(1, 2, 2);
-	mmm.set(2, 1, 3);
-	mmm.set(2, 2, 4);
-	nnn.set(1, 1, 1);
-	nnn.set(1, 2, 2);
-	nnn.set(2, 1, 3);
-	nnn.set(2, 2, 4);
-	mmm = mmm * nnn;
-	mmm.print();
-	nnn.print();
-	mmm = mmm | nnn;
-	mmm.print();
+	matrix<int, 2, 1> mmm;
+	mmm.set(0, 0, 1);
+	mmm.set(1, 0, 3);
+	matrix<int, 1, 2> nnn;
+	nnn.set(0, 0, 4);
+	nnn.set(0, 1, 3);
+	matrix<int, 2, 2> sss = mmm*nnn;
+	sss.print();
+	matrix<int, 2, 1> mmm1;
+	mmm1.set(0, 0, 11);
+	mmm1.set(1, 0, 31);
+	matrix<int, 2, 2> konk = mmm | mmm1;
+	konk.print();
 
-	for (matrix<int>::itr iterator; *iterator != mmm.end(); ++iterator) {
-		cout << *iterator << endl;
+	auto it = konk.begin();
+	for (; it != konk.end(); ++it) {
+		cout << *it << endl;
 	}
 
-	//	matrix<rational> mrrr; - конструктор по умолчанию тоже должен быть
-	//= matrix(3, 3);
-	//mrrr.print();
-	//std::cout << std::endl << std::endl;
-
-	//matrix mrrr2 = matrix(3, 3);
-	//mrrr2.print();
-	//std::cout << std::endl << std::endl;
-
-	//try
-	//{
-
-	//	matrix mrrr3 = mrrr + mrrr2;
-	//	if (mrrr.m != mrrr2.m || mrrr.n != mrrr2.n) throw mrrr3;
-	//	cout << "addition:" << endl;
-	//	mrrr3.print();
-
-	//	std::cout << std::endl;
-	//	std::cout << std::endl;
-
-	//	mrrr3 = mrrr3.rotate();
-	//	cout << "rotate:" << endl;
-	//	mrrr3.print();
-
-	//	std::cout << std::endl;
-	//	std::cout << std::endl;
-
-	//	rational r = mrrr3.get(2, 3);
-	//	cout << "Element with index 2,3:" << endl;
-	//	r.print();
-
-	//	std::cout << std::endl;
-	//	std::cout << std::endl;
-
-	//	mrrr3 = mrrr * mrrr2;
-	//	if (mrrr.m != mrrr2.m || mrrr.n != mrrr2.n) throw mrrr3;
-	//	cout << "Multiply:" << endl;
-	//	mrrr3.print();
-
-	//	std::cout << std::endl;
-	//	/*rational r = rational(13, 123);
-	//	rational p = rational(16, 123);
-	//	r = r * p;
-	//	r.print();*/
-
-	//}
-	//catch (matrix mat)
-	//{
-	//	std::cout << "Error in matrix row or column!" << std::endl;
-	//	return 0;
-	//}
-
-	//system("pause");
 	return 0;
 }
